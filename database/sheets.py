df78433d42112e96852f791a4dc6af75ded5571a
Sebastian Velez <sebastianvl1997@gmail.com>
2026-06-05 16:58:32 -0500

    Support Streamlit secrets

--- database/sheets.py
+++ database/sheets.py
@@ -1,12 +1,25 @@
+import os
 import gspread
+import streamlit as st
 
 SPREADSHEET_ID = "1ny7DoyHOd4MipYtBWew9EoQYiXFgvgyaerGRZVhH3UQ"
 
 def connect():
-    gc = gspread.service_account(
-        filename="credentials/service_account.json"
+
+    if os.path.exists(
+        "credentials/service_account.json"
+    ):
+
+        gc = gspread.service_account(
+            filename="credentials/service_account.json"
+        )
+
+    else:
+
+        gc = gspread.service_account_from_dict(
+            st.secrets["gcp_service_account"]
+        )
+
+    return gc.open_by_key(
+        SPREADSHEET_ID
     )
-
-    spreadsheet = gc.open_by_key(SPREADSHEET_ID)
-
-    return spreadsheet
