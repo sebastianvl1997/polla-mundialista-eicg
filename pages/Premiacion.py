388b607a9b1d716deaf169956bea14cd375071e8
Sebastian Velez <sebastianvl1997@gmail.com>
2026-06-23 02:04:13 -0500

    Premiacion

--- pages/Premiacion.py
+++ pages/Premiacion.py
@@ -8,46 +8,47 @@ Created on Tue Jun 23 01:47:07 2026
 import streamlit as st
 
 
+
 st.title("🏆 Premiación")
 
-    # st.markdown("""
-    # <style>
-    # .premiacion-title {
-    #     text-align: center;
-    #     font-size: 40px;
-    #     font-weight: 800;
-    #     color: #FFD700;
-    #     margin-bottom: 10px;
-    # }
+st.markdown("""
+<style>
+.premiacion-title {
+    text-align: center;
+    font-size: 40px;
+    font-weight: 800;
+    color: #FFD700;
+    margin-bottom: 10px;
+}
 
-    # .premiacion-sub {
-    #     text-align: center;
-    #     font-size: 18px;
-    #     color: #ddd;
-    #     margin-bottom: 30px;
-    # }
-    # </style>
-    # """, unsafe_allow_html=True)
+.premiacion-sub {
+    text-align: center;
+    font-size: 18px;
+    color: #ddd;
+    margin-bottom: 30px;
+}
+</style>
+""", unsafe_allow_html=True)
 
-    # st.markdown('<div class="premiacion-title">¡LA GLORIA TIENE SUS RECOMPENSAS!</div>', unsafe_allow_html=True)
-    # st.markdown('<div class="premiacion-sub">Los mejores se llevan los mejores premios</div>', unsafe_allow_html=True)
+st.markdown('<div class="premiacion-title">¡LA GLORIA TIENE SUS RECOMPENSAS!</div>', unsafe_allow_html=True)
+st.markdown('<div class="premiacion-sub">Los mejores se llevan los mejores premios</div>', unsafe_allow_html=True)
 
-    # IMAGEN PRINCIPAL DEL PODIO
+# IMAGEN PRINCIPAL DEL PODIO
 st.image("assets/premiacion.png", use_container_width=True)
 
-    # st.markdown("---")
+st.markdown("---")
 
-    # col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
-    # with col1:
-    #     st.metric(label="🥇 1er Puesto", value="$700.000")
+with col1:
+    st.metric(label="🥇 1er Puesto", value="$700.000")
 
-    # with col2:
-    #     st.metric(label="🥈 2do Puesto", value="$100.000")
+with col2:
+    st.metric(label="🥈 2do Puesto", value="$100.000")
 
-    # with col3:
-    #     st.metric(label="🥉 3er Puesto", value="$50.000")
+with col3:
+    st.metric(label="🥉 3er Puesto", value="$50.000")
 
-    # st.markdown("---")
+st.markdown("---")
 
-    # st.success("🎉 ¡Que gane el mejor y empiece la fiesta mundialista!")
+st.success("🎉 ¡Que gane el mejor y empiece la fiesta mundialista!")
